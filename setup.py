62bc88d418861a2fce4e0145646b22385e0f2529
kiki-kanri <a470666@gmail.com>
2022-11-19 20:46:18 +0800

    Create base files

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='kiki_utils_image',
+    classifiers=[
+        'License :: Freely Distributable'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    version='1.0.0',
+    description='Utils image functions.',
+    author='kiki-kanri',
+    author_email='a470666@gmail.com',
+    keywords=['Utils'],
+    install_requires=[],
+    python_requires=">=3.6"
+)
